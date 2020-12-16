2bde931d0da56bd6e679203b39f166433a3704cf
Stephanie I <72206861+StephanieIlievaa@users.noreply.github.com>
2020-12-16 20:25:01 +0200

    update

--- ExerciseArraysAdvanced.js/sorting.js
+++ ExerciseArraysAdvanced.js/sorting.js
@@ -1,16 +1,25 @@
 function sorting(input) {
+    let array = input.map(Number).sort(function (a, b) {
+        return a - b;
+    });
+    let arrayCopy = array.slice(0);
+    let result = [];
 
-    input.sort(customSort);
-
-    function customSort(a, b) {
-        if (a > b) {
-            return 1;
-        } else if (a < b) {
-            return -1;
-        } else {
-            return 0;
+    for (let i = 0; i < array.length; i++) {
+        result.push(arrayCopy[arrayCopy.length - 1]);
+        if (result.length === array.length) {
+            break;
         }
+        result.push(arrayCopy[0]);
+
+        if (result.length === array.length) {
+            break;
+        }
+        arrayCopy.shift();
+
+        arrayCopy.pop();
     }
 
+    console.log(result.join(" "));
 }
 sorting([1, 21, 3, 52, 69, 63, 31, 2, 18, 94]); 
