21a5653ff16e96f931e6c737c1ec9adc712f6f92
Stephanie I <72206861+StephanieIlievaa@users.noreply.github.com>
2021-01-11 17:36:37 +0200

    update

--- ExerciseArrays.js/addOrSubtract.js
+++ ExerciseArrays.js/addOrSubtract.js
@@ -1,5 +1,23 @@
-function solve(){
-    
+function solve(input) {
+    let array = input;
+    let newArray = [];
+    let firstArrSum = 0;
+    let secondArrSum = 0;
 
+    for(let i = 0; i < array.length; i ++){
+        if(array[i] % 2 == 0){
+           newArray.push(array[i] + i);
+           secondArrSum += array[i] + i;
+        }
+        else{
+            newArray.push(array[i] - i);
+            secondArrSum += array[i] - i;
+        }
+        firstArrSum += array[i];
+    }
+    console.log(newArray);
+    console.log(firstArrSum);
+    console.log(secondArrSum);
 }
-solve([5,15,56,35]);
+
+solve([5, 15, 23, 56, 35]);
